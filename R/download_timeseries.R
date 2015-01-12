55a8d30368851fc7573766d18ff767dc11dfbceb
Jordan S Read <jread@usgs.gov>
2015-01-12 09:13:29 -0600

    main methods in mda.streams

--- R/download_timeseries.R
+++ R/download_timeseries.R
@@ -14,30 +14,7 @@
 #'@export
 download_timeseries = function(site, variable, destination = NULL, session = NULL){
   
-  if (is.null(destination)){
-    file_handle = tempfile(fileext = '.tsv')
-  } else {
-    file_handle = paste0(destination,site,'.tsv')
-  }
-  
-  type <- paste0('ts_', variable)
-  
-  item <- query_item_identifier(scheme = 'mda_streams', type = type, key = site, session = session, limit = 5)
-  id <- item$id
-  
-  file_names <- item_list_files(id, session)
-  if (nrow(file_names) > 1){
-    stop('item ', item, ' has more than one associated file for ', variable)
-  }
-  
-  file_get <- item_file_download(id = id, names = file_names$fname, destination = file_handle, session = session)
-  
-  if (file_get){
-    return(file_handle)
-  } else {
-    warning('file failed to download')
-    return(NULL)
-  }
+  file_handle <- download_ts(site = site, variable = variable, destination = destination, session = session)
   
   
 }
