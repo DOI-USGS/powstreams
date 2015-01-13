79853dd174bed5db3088b11d6a652fa149ea800d
Jordan S Read <jread@usgs.gov>
2015-01-12 20:08:25 -0600

    removed big query from test

--- tests/testthat/test-sites.R
+++ tests/testthat/test-sites.R
@@ -1,8 +1,6 @@
 context("list sites")
 
 test_that("listing variables from site fails", {
-  sites <- list_sites()
-  list_timeseries(site = sites[1])
   expect_error(list_timeseries(site = 'bad_site_name'))
   expect_error(download_timeseries(site = 'bad_site_name', variable = 'doobs'))
   expect_error(load_timeseries(site = 'bad_site_name', variable = 'doobs'))
