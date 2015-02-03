435bc5258f694e034f03ad9ac69a6a28f820872a
Jordan S Read <jread@usgs.gov>
2015-02-03 09:04:50 -0600

    test for list_sites(with..)

--- tests/testthat/test-sites.R
+++ tests/testthat/test-sites.R
@@ -6,6 +6,10 @@ test_that("listing variables from site fails", {
   expect_error(load_timeseries(site = 'bad_site_name', variable = 'doobs'))
   expect_error(download_watershed(site = 'bad_site_name'))
   
+  expect_is(list_sites(), 'character')
+  expect_is(list_sites(with_timeseries = 'wtr'), 'character')
+  expect_is(list_sites(with_timeseries = c('wtr', 'doobs')), 'character')
+  
   expect_is(download_watershed(site = "nwis_01408500"), 'character')
   expect_is(load_timeseries(site = "nwis_01408500", variable = 'doobs'), 'data.frame')
   
