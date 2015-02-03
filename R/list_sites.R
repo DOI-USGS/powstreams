3abcf90ecffc61b226d3d1c260fa26258382ab97
Jordan S Read <jread@usgs.gov>
2015-02-03 03:57:32 -0600

    updated list_sites

--- R/list_sites.R
+++ R/list_sites.R
@@ -1,7 +1,7 @@
 #'@title get site data from a powstreams site
 #'@description returns data from site into R data.frame
 #'
-#'@param with_timeseries FALSE for all sites, or a valid polygon
+#'@param with_timeseries NULL for all sites, or a valid timeseries name
 #'@param session a valid sciencebase session (see \code{\link[sbtools]{authenticate_sb}}). 
 #'Set \code{session = NULL} (default) for sites on sciencebase that are public.
 #'@return a character vector of powstreams sites according to user-specified parameters
@@ -9,7 +9,7 @@
 #'@examples
 #'list_sites() # get all sites
 #'\dontrun{
-#'list_sites(with_timeseries = 'wtr') # get all sites within a user specific polygon (inclusive)
+#'list_sites(with_timeseries = 'wtr') # get all sites with a given type of data
 #'}
 #'@import mda.streams
 #'@import sbtools
@@ -27,8 +27,11 @@ list_sites <- function(with_timeseries = NULL, session = NULL){
     
     sites <- vector(mode = 'character', length = nrow(ids))
     for (i in 1:nrow(ids)){
-      sites[i] <- item_get_parent(ids[i, 2], session = session)
+      site_id <- item_get_parent(ids[i, 2], session = session)
+      sites[i] <- mda.streams:::get_title(site_id, session)
     }
+    
+    # get sites that are repeated as many times as the number of types used
     tbl_sites <- data.frame(table(sites))
     sites <- as.character(tbl_sites$sites[tbl_sites$Freq == length(types)])
   }
