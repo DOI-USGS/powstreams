cff0d5512c4c546d62536181f66a1e730a036d2b
Alison Appling <aappling@usgs.gov>
2015-06-02 14:19:17 -0500

    use new mda.streams functions

--- R/list_sites.R
+++ R/list_sites.R
@@ -1,34 +1,21 @@
-#'@title get site data from a powstreams site
-#'@description returns data from site into R data.frame
-#'
-#'@param with_timeseries NULL for all sites, or a valid timeseries name
-#'@param session a valid sciencebase session (see \code{\link[sbtools]{authenticate_sb}}). 
-#'Set \code{session = NULL} (default) for sites on sciencebase that are public.
-#'@return a character vector of powstreams sites according to user-specified parameters
-#'
-#'@examples
-#'list_sites() # get all sites
-#'\dontrun{
-#'list_sites(with_timeseries = 'wtr') # get all sites with a given type of data
-#'}
-#'@importFrom mda.streams get_sites make_ts_variable
-#'@export
-list_sites <- function(with_timeseries = NULL, session = NULL){
-  
-  if(is.null(with_timeseries)){
-    
-    sites <- get_sites(session = session)
-    
-  } else {
-    types <- make_ts_variable(variable = with_timeseries)
-    sites <- vector('character')
-    for (k in 1:length(types)){
-      sites <- append(sites, get_sites(with_child_key = types[k], session = session))
-    }
-    # get sites that are repeated as many times as the number of types used
-    tbl_sites <- data.frame(table(sites))
-    sites <- as.character(tbl_sites$sites[tbl_sites$Freq == length(types)])
-  }
-
-  return(sites)
+#' @title get site data from a powstreams site
+#' @description returns data from site into R data.frame
+#'   
+#' @param with_timeseries NULL to return all sites, or character vector of
+#'   timeseries variables (i.e., 1+ of those listed in get_ts_variables())
+#' @param logic how to join the constraints in with_timeseries, ...: is any of 
+#'   the listed parameters sufficient, or do you need all of them to be 
+#'   available for a site to qualify?
+#' @return a character vector of site IDs
+#' @examples
+#' list_sites() # get all sites
+#' \dontrun{
+#' list_sites(with_timeseries = 'wtr') # get all sites with a given type of data
+#' list_sites(with_timeseries = c('wtr','doobs')) # get all sites with both wtr and doobs data
+#' list_sites(with_timeseries = c('wtr','doobs'), logic='any') # get all sites with EITHER wtr or doobs data (or both)
+#' }
+#' @importFrom mda.streams find_sites
+#' @export
+list_sites <- function(with_timeseries, logic=c("all","any"), ...){
+  find_sites(with_timeseries=with_timeseries, logic=logic, ...)
 }
--- R/site_location.R
+++ R/site_location.R
@@ -1,7 +1,7 @@
 #'@title get site latitude and longitude 
 #'@param site a valid powstreams site (see \code{\link{list_sites}})
 #'
-#'@importFrom mda.streams split_site
+#'@importFrom mda.streams find_site_coords
 #'@importFrom dataRetrieval readNWISsite
 #'@examples
 #'site_location("nwis_11126000")
@@ -10,14 +10,5 @@
 #'}
 #'@export
 site_location <- function(site){
-  nwis_site <- split_site(site)
-  site_data <- readNWISsite(nwis_site)
-  lat <- site_data$dec_lat_va
-  lon <- site_data$dec_long_va
-  location <- data.frame('longitude' = lon, 'latitude' = lat)
-  
-  if (length(site) > 1){
-    location <- cbind(data.frame('site' = site_data$site_no), location)
-  }
-  return(location)
+  find_site_coords(site)
 }
