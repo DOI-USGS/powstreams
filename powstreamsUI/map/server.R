0fff1f8aa187fd796aa8c7734ca8128896ec2232
Jordan S Read <jread@usgs.gov>
2015-07-17 07:50:05 -0500

    variable selectors and histogram

--- powstreamsUI/map/server.R
+++ powstreamsUI/map/server.R
@@ -5,9 +5,11 @@ server <- function(input, output, session) {
   pal <- colorQuantile("YlOrRd", NULL, n = 8)
   
   points <- eventReactive(input$recalc, {
-    site_data = v(get_meta(types = c("basic"), out = c("lat","lon","alt","long_name","site_name")))
+    site_data = v(get_meta(types = c("basic")))
     site_data[rowSums(is.na(site_data)) == 0,]
   }, ignoreNULL = FALSE)
+  
+  
   
   output$mymap <- renderLeaflet({
     data <- points()
@@ -15,7 +17,17 @@ server <- function(input, output, session) {
       addProviderTiles("Stamen.TonerLite",
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addCircleMarkers(data = data, radius = 10, color=~pal(alt), 
-                       popup = ~paste0(long_name, '<br/>', sprintf("<a href='http://waterdata.usgs.gov/usa/nwis/uv?site_no=%s'>%s</a>",sapply(site_name, function(x)strsplit(x,'[_]')[[1]][2]), site_name), '<br/> elevation:',alt))
+      addCircleMarkers(data = data, radius = 10, color=~pal(data[[input$variable]]), 
+                       popup = ~paste0(long_name, '<br/>', 
+                                       sprintf("<a href='http://waterdata.usgs.gov/usa/nwis/uv?site_no=%s'>%s</a>",sapply(site_name, function(x)strsplit(x,'[_]')[[1]][2]), site_name), '<br/> value:',data[[input$variable]]))
+  })
+  
+  output$histogram <- renderPlot({
+    data <- points()
+    # Render a barplot
+    hist(data[[input$variable]],
+            ylab="Number",
+            xlab=input$variable,
+         main = input$variable)
   })
 }
--- powstreamsUI/map/ui.R
+++ powstreamsUI/map/ui.R
@@ -4,8 +4,21 @@ library(leaflet)
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
-ui <- fluidPage(
-  leafletOutput("mymap"),
-  p(),
-  actionButton("recalc", "New points")
-)
+shinyUI(fluidPage(
+  
+  titlePanel("Powstreams working group"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("variable", label = "Sites to visualize", 
+                  choices = names(mda.streams::get_meta(types = c("basic"))),
+                  selected = "alt", multiple = FALSE),
+      actionButton("recalc", "Submit")
+    ),
+    mainPanel(
+      leafletOutput("mymap"),
+  plotOutput("histogram"), 
+  p()
+    )
+  )
+))
