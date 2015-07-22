9ef8d84f4a54364a4e3db5c246748d1a5af2afec
Jordan S Read <jread@usgs.gov>
2015-07-22 11:44:09 -0500

    refactoring shiny code

--- inst/shiny/timeseries/server.R
+++ inst/shiny/timeseries/server.R
@@ -1,60 +1,46 @@
 
-shinyServer(function(input, output) {
+noneselected = "-- no variable selected --"
+
+library(shiny)
+shinyServer(function(input, output, session) {
   
-  downloaded <- function(i){
-    
-    if (i <= length(input$dataset)){
-      data = xts::xts(NULL)
-      metadata <- mda.streams::get_var_src_codes(var_src==input$dataset[i], out=c('units','var_descrip','var'))
-      ylab <- paste0(metadata$var_descrip, ' (',metadata$units,')')
-      
-      for (site in input$site){
-        rdfile <- file.path(tempdir(),paste0(site,'-ts_',input$dataset[i],'.RData'))
-        if (file.exists(rdfile)){
-          message("file is cached, using local:", input$dataset[i])
-          load(file = rdfile) # loads tsobject
-        } else {
-          message("downloading file:", input$dataset[i])
-          file <- mda.streams::download_ts(var_src = input$dataset[i], site_name = site, on_local_exists = 'skip', on_remote_missing = "return_NA")
-          if (is.na(file))
-            tsobject <- xts::xts(NULL)
-          else {
-            unitt <- read_ts(file)
-            var <- unitted::v(unitt)
-            tsobject <- xts::xts(var[[2]], order.by = as.POSIXct(var[['DateTime']]), tz='UTC')
-          }
-          save(tsobject, list='tsobject', file = rdfile)
-        }
-        
-        oldnames <- names(data)
-        data <- merge(data, tsobject)
-        if(length(tsobject) > 0)
-          names(data) <- c(oldnames,paste0(site,' (',metadata$units,')'))
-        
-      }
-      list(data=data, ylab=ylab, units = metadata$units, var=metadata$var)
+  
+  
+  output$Box1 = renderUI(
+    if (is.null(input$site)){
+      return()
     } else {
-      NULL
-    }# // else do nothing
-  }
-  
-  buildDy <- function(i){
-    ts <- downloaded(i)
-    if (!is.null(ts) && length(ts$data) > 0 ){
-      dygraphs::dygraph(ts$data, group = "powstreams") %>%
-        dygraphs::dyOptions(colors = RColorBrewer::brewer.pal(max(3,length(names(ts$data))), "Dark2")) %>%
-        dygraphs::dyHighlight(highlightSeriesBackgroundAlpha = 0.65, hideOnMouseOut = TRUE) %>%
-        dygraphs::dyAxis("y", label = ts$ylab)
+      all.datasets = unique(unlist(lapply(input$site,mda.streams::list_datasets)))
+      selectizeInput("dataset1","Variables to visualize",
+                     c(all.datasets, noneselected),
+                     multiple = FALSE, selected=noneselected)
     }
-  }
+  )
   
-  output[['dygraph1']] <- renderDygraph({
-    buildDy(1)
+  output$Box2 = renderUI(
+    if (is.null(input$dataset1) || input$dataset1 == noneselected){
+        return()
+    } else {
+      all.datasets = unique(unlist(lapply(input$site,mda.streams::list_datasets)))
+      selectInput("dataset2", label=NULL,
+                       c(all.datasets[-which(all.datasets %in% input$dataset1)],noneselected),
+                       multiple = FALSE, selected=noneselected)
+    }
+  )
+  
+  output$Box3 = renderUI(
+    if (is.null(input$dataset2) || input$dataset2 == noneselected){
+        return()
+    } else {
+      all.datasets = unique(unlist(lapply(input$site,mda.streams::list_datasets)))
+      selectInput("dataset3", label=NULL,
+                  c(all.datasets[-which(all.datasets %in% c(input$dataset1,input$dataset2))],noneselected),
+                  multiple = FALSE, selected=noneselected)
+    }
+  )
+  
+  observeEvent(input$render, {
+    output$Box4 <-renderText(paste(c(input$dataset1, input$dataset2, input$dataset3)))
   })
-  output[['dygraph2']] <- renderDygraph({
-    buildDy(2)
-  })
-  output[['dygraph3']] <- renderDygraph({
-    buildDy(3)
-  })
+  
 })
--- inst/shiny/timeseries/ui.R
+++ inst/shiny/timeseries/ui.R
@@ -1,25 +1,49 @@
-library(dygraphs)
-initial.site = "nwis_06893820"
-initial.var = "doobs_nwis"
+# 
 
+# 
+# shinyUI(fluidPage(
+#   
+#   titlePanel("Powstreams working group"),
+#   
+#   sidebarLayout(
+#     sidebarPanel(
+#       selectInput("site", label = "Sites to visualize", 
+#                   choices = sites.list,
+#                   selected = initial.site, multiple = TRUE),
+#       selectizeInput("dataset", label = "Timeseries dataset",
+#                   choices = mda.streams::list_datasets(initial.site),
+#                   selected = initial.var, multiple = TRUE, options = list(maxItems = 3)),
+#       submitButton("Submit")
+#     ),
+#     mainPanel(
+#       fluidRow(column(3, verbatimTextOutput("dataset")))
+#     )
+#   )
+# ))
+
+initial.site = "nwis_06893820"
+
+sites.list <- mda.streams::list_sites()
+library(shiny)
+library(dygraphs)
 shinyUI(fluidPage(
-  
   titlePanel("Powstreams working group"),
   
   sidebarLayout(
     sidebarPanel(
       selectInput("site", label = "Sites to visualize", 
-                  choices = mda.streams::list_sites(),
-                  selected = initial.site, multiple = TRUE),
-      selectizeInput("dataset", label = "Timeseries dataset",
-                  choices = mda.streams::list_datasets(initial.site),
-                  selected = initial.var, multiple = TRUE, options = list(maxItems = 3)),
-      submitButton("Submit")
+                                     choices = sites.list,
+                                     selected = initial.site, multiple = TRUE),
+      uiOutput("Box1"),
+      uiOutput("Box2"),
+      uiOutput("Box3"),
+      actionButton("render", "Render visuals")
+      
     ),
-    mainPanel(
-      dygraphOutput("dygraph1"),
-      dygraphOutput("dygraph2"),
-      dygraphOutput("dygraph3")
-    )
+    mainPanel("Display results",
+              uiOutput("Box4"),
+              dygraphOutput("dygraph1"),
+              dygraphOutput("dygraph2"),
+              dygraphOutput("dygraph3")
   )
-))
+)))
