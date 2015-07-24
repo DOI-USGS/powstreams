21934b5cda2d99f2bf1b8eddd9398ef4c2bda1e7
Jordan S Read <jread@usgs.gov>
2015-07-24 11:45:53 -0500

    removing hack and adding support for other data

--- R/shiny_helpers.R
+++ R/shiny_helpers.R
@@ -3,7 +3,7 @@
 #' @importFrom mda.streams get_meta
 #' @keywords internal
 get_types <- function(){
-  meta_types = get_meta(types = c("basic"))
+  meta_types = get_meta()
   
   names(which(sapply(meta_types, class) == 'unitted_numeric'))
 }
