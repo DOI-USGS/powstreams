146a9155baa728d966492d5d1a3b9bfda35404b8
Alison Appling <aappling@usgs.gov>
2015-07-31 14:39:51 -0500

    add view_file + view_folder

---
+++ R/view_file.R
@@ -0,0 +1,11 @@
+#' Open a file
+#' 
+#' Opens the specified file using the default program for that file type
+#' 
+#' @param path a file path
+#'   
+#' @importFrom mda.streams view_file
+#' @name view_file
+#' @rdname view_file
+#' @export
+NULL
