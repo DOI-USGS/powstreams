163d3695d573ee07f18a3b4f525df4830ba904f8
Jordan S Read <jread@usgs.gov>
2015-07-31 10:14:21 -0500

    starting to simplify content

--- inst/shiny/model/server.R
+++ inst/shiny/model/server.R
@@ -2,7 +2,9 @@ library(dygraphs)
 library(datasets)
 library(DT)
 library(powstreams)
-metab_models = add_rownames(parse_metab_model_name(list_metab_models()),'model_name') %>% as.data.frame()
+
+noneselected = "-- no variable selected --"
+metab_models = dplyr::add_rownames(mda.streams::parse_metab_model_name(mda.streams::list_metab_models()),'model_name') %>% as.data.frame()
 
 
 shinyServer(function(input, output) {
@@ -10,20 +12,26 @@ shinyServer(function(input, output) {
   
   get_model_data <- reactive({
   
-    ts.out <- list(GPP=xts::xts(NULL), ER=xts::xts(NULL), K600=xts::xts(NULL))
+    empty.xts = xts::xts(c(NA,NA), order.by = as.POSIXct(c('2011-01-01','2011-01-02')))
+    ts.out <- list(GPP=empty.xts, ER=empty.xts, K600=empty.xts)
     if (length(input$x1_rows_selected)>0){
+      
       for (row in input$x1_rows_selected){
         file = download_metab_model(metab_models[row, ]$model_name, on_local_exists = 'skip')
         if (file.size(file) > 0){
           load(file) # site-specific, run-specific model object
-          for (i in names(ts.out)){
+          fit.types = names(mm@fit)[lapply(mm@fit, class) == 'numeric']
+          for (i in fit.types){
             val <- data.frame(xts::xts(x = mm@fit[[i]], order.by = as.POSIXct(mm@fit$date)))
-            val = setNames(val, metab_models[row, ]$model_name)
-            ts.out[[i]] = merge(ts.out[[i]], val)
+            val <- setNames(val, metab_models[row, ]$model_name)
+            if (all(is.na(ts.out[[i]]))) # cover case where the var is not defined in the list
+              ts.out[[i]] = val
+            else
+              ts.out[[i]] = merge(ts.out[[i]], val)
           }
         } else {
           for (i in names(ts.out)){
-            val <- xts::xts(c(NA,NA), order.by = as.POSIXct(c('2011-01-01','2011-01-02')))
+            val <- empty.xts
             setNames(val, metab_models[row, ]$model_name)
             ts.out[[i]] = merge(ts.out[[i]], val)
             
@@ -32,9 +40,9 @@ shinyServer(function(input, output) {
         
       }
     } else { # // nothing selected
-      ox = xts::xts(c(NA,NA), order.by = as.POSIXct(c('2011-01-01','2011-01-02')))
+      
       for (i in names(ts.out)){
-        ts.out[[i]] = data.frame(ox)
+        ts.out[[i]] = data.frame(empty.xts)
         names(ts.out[[i]]) <- i
       }
     }
@@ -46,25 +54,7 @@ shinyServer(function(input, output) {
   output$x1 = DT::renderDataTable(metab_models[,c(-1,-2)], server = FALSE, rownames=FALSE, #filter='top',
                                   options=list(pageLength=15,
                                                order=list(list(2,'desc'))))
-  
-  # highlight selected rows in the scatterplot
-  output$x2 = renderPlot({
-    s = input$x1_rows_selected
-    par(mar = c(4, 4, 1, .1))
-    plot(cars)
-    if (length(s)) points(cars[s, , drop = FALSE], pch = 19, cex = 2)
-  })
-  
-  
-  output$Box1 <- renderUI({
-    # default to median of data
-    sliderInput("min", "minimum threshold:", 
-                min = min(model@fit[[c('r.squared')]], na.rm=T), max = max(model@fit[[c('r.squared')]], na.rm=T), 
-                value = c(min(model@fit[[c('r.squared')]], na.rm=T), floor(quantile(model@fit[[c('r.squared')]], na.rm=T)[[3]])), step= 0.05)
-  })
-  
 
-  
   colors = RColorBrewer::brewer.pal(5, "Dark2")
   output$dygraph1 <- renderDygraph({
     data = get_model_data()[["GPP"]]
