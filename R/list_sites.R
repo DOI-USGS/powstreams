5e500b0336bcbaee4965116fc570fb047b2d6096
Alison Appling <aappling@usgs.gov>
2015-07-31 13:06:11 -0500

    add list_sites

--- R/list_sites.R
+++ R/list_sites.R
@@ -1,9 +1,12 @@
-#' Higher-level site querying function
+#' Get a list of the sites on ScienceBase
 #' 
-#' @param with_var_src character vector of data variables (i.e., 1+ of those
+#' The with_var_src argument optionally limits the list to those sites that
+#' contain specific timeseries variables.
+#' 
+#' @param with_var_src character vector of data variables (i.e., 1+ of those 
 #'   listed in get_var_src_codes(out='var_src'))
-#' @param logic how to join the constraints in with_var_src, ...: is any of the
-#'   listed parameters sufficient, or do you need all of them to be available
+#' @param logic how to join the constraints in with_var_src, ...: is any of the 
+#'   listed parameters sufficient, or do you need all of them to be available 
 #'   for a site to qualify?
 #' @param ... additional querying arguments yet to be implemented
 #' @return a character vector of site IDs
