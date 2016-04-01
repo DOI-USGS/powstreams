feafea543590e81796fc1c49a365870acfc0d2e5
Alison Appling <aappling@usgs.gov>
2016-04-01 15:15:45 -0500

    give get_ts_metadata the power of get_var_src_codes

--- R/get_ts_metadata.R
+++ R/get_ts_metadata.R
@@ -1,7 +1,12 @@
 #' Get information about timeseries variables and sources
 #' 
+#' @param ... a list of unnamed arguments to pass to \code{dplyr::filter_}
+#' @param out a list of arguments to pass to \code{dplyr::select_}
+#' @param drop logical. if the filtering & selection lead to a data.frame whose 
+#'   dimensions would ordinarily be dropped using z[x,y] location, should the 
+#'   dimensions be dropped?
 #' @importFrom mda.streams get_var_src_codes
 #' @export
-get_ts_metadata <- function() {
-  get_var_src_codes(out=c('var','src','units','metab_var','var_descrip','src_descrip'))
+get_ts_metadata <- function(..., out=c('var','src','units','metab_var','var_descrip','src_descrip'), drop=TRUE) {
+  get_var_src_codes(..., out=out, drop=drop)
 }
